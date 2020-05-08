784a80b7e68870a71724d9e2120e8c38f5bd6f61
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2020-05-08 12:24:48 +0100

    Initial Commit

---
+++ Weather App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather App
+//
+//  Created by Oliver Curtis on 08/05/2020.
+//  Copyright © 2020 Oliver Curtis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
